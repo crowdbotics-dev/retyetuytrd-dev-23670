ed9bf7a6d019152274452ca82896664771233b81
Crowdbotics <engineering@crowdbotics.com>
2022-03-11 07:27:34 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Homme1
+
+admin.site.register(Homme1)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Homme1(models.Model):
+    "Generated Model"
+    phone = models.BigIntegerField()
